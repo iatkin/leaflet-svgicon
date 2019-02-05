4a52ca7c97ff851066c945b11a7852c06b75157e
iatkin <ilya.atkin@unh.edu>
2019-02-05 13:25:37 -0500

    Update subclass-examples.js

--- subclass-examples.js
+++ subclass-examples.js
@@ -26,8 +26,8 @@ L.DivIcon.SVGIcon.RhombusIcon = L.DivIcon.SVGIcon.extend({
         var fontWeight = this.options.fontWeight
         var lineHeight = Number(this.options.fontSize)
 
-        var x = Number(this.options.iconSize.x) / 2
-        var y = Number(this.options.iconSize.y)/ 2 + (lineHeight * 0.35)
+        var x = this.options.circleAnchor.x
+        var y = this.options.circleAnchor.y + (lineHeight * 0.35)
         var circleText = this.options.circleText
         var textColor = this.options.fontColor.replace("rgb(", "rgba(").replace(")", "," + this.options.fontOpacity + ")")
 
